600f52d0adc781c210ecde552437dc08f00eff48
Wei Kang Wong <wwk97@hotmail.sg>
2023-11-17 18:27:19 +0200

    preprocessing and mel spec extraction

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,48 @@
+import argparse
+import pickle
+from pathlib import Path
+
 import numpy as np
-import pandas as pd
-import os
+from tqdm import tqdm
 
+from AudioLibrary.AudioFeatures import AudioFeatures
+from AudioLibrary.AudioSignal import AudioSignal
 
-print(os.getcwd())
-
-df = pd.read_csv(r'data\URBAN-SED_v2.0.0\annotations\train\soundscape_train_unimodal1999.txt')
-print(df)
+# Audio features extraction function
+def global_feature_statistics(
+        y,
+        win_size=0.025,
+        win_step=0.01,
+        nb_mfcc=12,
+        mel_filter=40,
+        stats=[
+            'mean',
+            'std',
+            'med',
+            'kurt',
+            'skew',
+            'q1',
+            'q99',
+            'min',
+            'max',
+            'range'
+        ],
+        features_list=[
+            'zcr',
+            'energy',
+            'energy_entropy',
+            'spectral_centroid',
+            'spectral_spread',
+            'spectral_entropy',
+            'spectral_flux',
+            'sprectral_rolloff',
+            'mfcc'
+        ]):
+    # Extract features
+    audio_features = AudioFeatures(y, win_size, win_step)
+    features, features_names = audio_features.global_feature_extraction(
+        stats=stats,
+        features_list=features_list,
+        nb_mfcc=nb_mfcc,
+        nb_filter=mel_filter)
+    return features
